150fbd5823c4ef621f7372575b7e425eb9571de1
minaekramy12 <minaekramy77@gmail.com>
2026-04-13 23:37:26 +0200

    Add: add node.h file

---
+++ Restaurant-Project/Node.h
@@ -0,0 +1,90 @@
+
+#ifndef NODE_H
+#define NODE_H
+
+template<typename T>
+class Node
+{
+private:
+	T item;
+	Node<T>* next;
+public:
+
+	Node()
+	{
+		next = nullptr;
+	}
+
+	Node(T newItem)
+	{
+		item = newItem;
+		next = nullptr;
+
+	}
+
+	void setItem(T newItem)
+	{
+		item = newItem;
+	}
+
+	void setNext(Node<T>* nextNodePtr)
+	{
+		next = nextNodePtr;
+	}
+
+	T getItem() const
+	{
+		return item;
+	}
+
+	Node<T>* getNext() const
+	{
+		return next;
+	}
+	virtual ~Node() {}
+};
+
+
+template < typename T>
+class priNode
+{
+private:
+	T item;
+	double pri;
+	priNode<T>* next;
+public:
+	priNode(const T& r_Item, double PRI)
+	{
+		setItem(r_Item, PRI);
+		next = nullptr;
+	}
+	void setItem(const T& r_Item, double PRI)
+	{
+		item = r_Item;
+		pri = PRI;
+	}
+	void setNext(priNode<T>* nextNodePtr)
+	{
+		next = nextNodePtr;
+	}
+
+	T getItem(double& PRI) const
+	{
+		PRI = pri;
+		return item;
+	}
+
+	priNode<T>* getNext() const
+	{
+		return next;
+	}
+
+	int getPri() const
+	{
+		return pri;
+	}
+};
+
+
+
+#endif
