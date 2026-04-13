346295264f9172bc19ca7b60add19079fca61bee
minaekramy12 <minaekramy77@gmail.com>
2026-04-13 23:38:49 +0200

    Add: add priorityQueue.h file

---
+++ Restaurant-Project/PriQueue.h
@@ -0,0 +1,78 @@
+#include "Node.h"
+#ifndef _PRI_QUEUE_H_
+#define _PRI_QUEUE_H_
+
+template <typename T>
+class priQueue
+{
+private:
+    priNode<T>* head;
+    int count;
+public:
+    priQueue() : head(nullptr), count(0) {}
+
+    ~priQueue() {
+        T tmp;
+        double p;
+        while (dequeue(tmp, p));
+    }
+
+    bool enqueue(const T& data, double priority) {
+        priNode<T>* newNode = new priNode<T>(data, priority);
+
+        if (head == nullptr || priority > head->getPri()) {
+
+            newNode->setNext(head);
+            head = newNode;
+            count++;
+            return true;
+        }
+
+        priNode<T>* current = head;
+        while (current->getNext() && priority <= current->getNext()->getPri()) {
+            current = current->getNext();
+        }
+        newNode->setNext(current->getNext());
+        current->setNext(newNode);
+        count++;
+        return true;
+    }
+
+    bool dequeue(T& topEntry, double& pri) {
+        if (isEmpty())
+            return false;
+
+        topEntry = head->getItem(pri);
+        priNode<T>* temp = head;
+        head = head->getNext();
+        delete temp;
+        count--;
+        return true;
+    }
+
+    T peekFront() const throw(PrecondViolatedExcep) {
+        if (isEmpty())
+            throw PrecondViolatedExcep("Priority Queue is empty");;
+
+        return head->getItem();
+    }
+
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+
+    int getCount() const { return count; }
+
+    void Print() const
+    {
+        priNode<T>*current = head;
+        while (current)
+        {
+            cout << current->getItem() << endl;
+            current = current->getNext();
+        }
+    }
+};
+
+
+#endif
